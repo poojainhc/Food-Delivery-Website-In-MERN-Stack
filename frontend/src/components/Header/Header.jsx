1ad4809d14f4336cf127e8312caae557b90d62d4
Pooja Jadhav <poojakhawle123@gmail.com>
2026-04-11 14:36:15 +0530

    Now Header is Caplial

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import { memo } from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-content">
+        <h2>Order your favorite meals now!
+        </h2>
+        <p>Delicious food delivered to your doorstep.</p>
+        <button className="view-menu-btn">View Menu</button>
+      </div>
+      
+      
+    </div>
+  );
+};
+
+export default memo(Header);
