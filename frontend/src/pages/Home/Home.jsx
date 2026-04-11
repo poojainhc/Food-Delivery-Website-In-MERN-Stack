320b7f532b1c7baa8f12c46cbc42ad269dc19901
Pooja Jadhav <poojakhawle123@gmail.com>
2026-04-11 10:52:02 +0530

    FIle header name updated

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import { memo, useState } from 'react';
 import './Home.css';
-import Header from '../../components/header/header';
+import Header from '../../components/Header/Header';
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay';
 import Appdownload from '../../components/Appdownload/Appdownload';
